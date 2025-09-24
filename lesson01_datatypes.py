e13dcdbb9ae68e313c4aa0ca1d068e0a4fe7f949
Julianf-30 <julian.l.fechner@gmail.com>
2025-09-24 16:18:01 +0000

    Created Datatypes lesson

---
+++ lesson01_datatypes.py
@@ -0,0 +1,42 @@
+# This is a commment (Use Hashtag)  
+#DATA TYPES
+
+# 1. STRINGS:
+print(" ")
+print("once upon a time i \npassed csaea and it was a great time for all")
+# use \n to make a line break
+print("Name: \tJulian \nAge: \t14 \nGrade: \t9")
+
+#INTEGERS
+
+print(42)
+print(-23)
+print(33+44-10)
+
+#FLOAT
+print(4.0)
+print(5.2)
+print(2.0+4.1)
+print(2.4+3)
+print(5.5-4.2221)
+print(5.22222+4.222221)
+
+#BOOLEAN
+print(True) #1
+print(False) #0
+print(True * False)
+print(False/True)
+print(1==2)
+print(2==2)
+print(1!=1)
+print(1!=2)
+print("Hello" == "Hello")
+print("Hello"=="Helo")
+print(5+5 ==11)
+
+#Check what type of data it is 
+
+print(type("the"))
+print(type(4))
+print(type(4.2221))
+print(type(2==1))
