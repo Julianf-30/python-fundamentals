abc8290d2af2630a15ac610b0c119e0e17762566
Julianf-30 <julian.l.fechner@gmail.com>
2025-09-25 17:26:04 +0000

    learned variables

---
+++ lesson02_variables.py
@@ -0,0 +1,67 @@
+#CONCEPTS VARIABLES 
+
+email = "fechner20290368@student.nhvweb.net"
+password = "password1234"
+print("Email: ", email)
+print("Password: ", password)
+
+Temperature = 72.5
+Price = 3.99
+print(type(Temperature))
+enabled = False 
+is_complete = True
+enabled=True 
+print(is_complete)
+print(enabled)
+
+# for math problems:
+
+x=3.14
+y=7
+print(x+y)
+
+#Varibales are felxivle. you create or update another variable, like so:
+
+count=10
+print(count)
+count_down= count-1
+print(count_down)
+count=count_down
+print(count)
+count_down = count-1
+print(count_down)
+count=count_down
+print(count)
+
+
+#Challenge 1: Rename Variables 
+#Change the Variable names x, y, z below to more descriptive names. 
+
+name= "Radia Perlman"
+Age= 34
+Occupation= "Newtworking Engineer"
+
+#Challenge 2: Update Variables 
+#Create a variable called 'count' with a value of 10
+#increase it by 5 and print the result
+
+count=10 
+print(count)
+count_up=count+5
+count=count_up
+print(count_up)
+
+#Challenge 3: Swap Variables 
+
+x=4, 
+y="hello" 
+z= y
+y=x
+x=z
+print(y,x)
+
+#Challenge 4
+
+is_raining= False
+is_raining=True 
+print("Is it raining", is_raining)
