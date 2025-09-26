1acb1e6c38ef8dd8ccf0fe09767f197591182181
Julianf-30 <julian.l.fechner@gmail.com>
2025-09-26 18:25:32 +0000

    did challenges for math

--- lesson03_math.py
+++ lesson03_math.py
@@ -42,3 +42,19 @@ result5=5+2**3*(4-1)
 print("Result 5:", result5)
 
 #CHALLENGES
+
+#Challenge 1: Rectangle Area 
+#Calcualte the are ofa rctangle with a width of 8 and a height of 5
+
+area_of_rectangle = 8*5
+print("Area of Rectangle: ", area_of_rectangle)
+
+circle_area= 3.14*7**2
+print("Circle area: ", circle_area)
+
+Total_cost= 12.99*3 + 3.5*4 
+print("Total Cost:", Total_cost)
+
+x=67%2
+print("Module: ", x)
+#The number has a remainder so it must be odd 
