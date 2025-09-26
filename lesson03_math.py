84b3567bd8d15e38c4a01d9bc1871dbb404a4ad4
Julianf-30 <julian.l.fechner@gmail.com>
2025-09-26 18:00:15 +0000

    learned basic math operators

---
+++ lesson03_math.py
@@ -0,0 +1,43 @@
+#KEY CONCEPTS: math operators +, -, *, /, //, %, **
+
+add = 34+33
+print("Sum: ", add)
+
+subtract = 100-33
+print("Difference: ", subtract)
+
+multiply = 15 * 2
+print("Product: ", multiply)
+
+float_divide= 67/2
+print("Quotient: ", float_divide)
+
+integer_divide = 67//2
+print("Quotient: ", integer_divide)
+
+modulus = 7%2
+print("Modulus: ", modulus)
+
+exponent=7**2
+print("exponent: ", exponent)
+
+print(2+2 == 10-6)
+
+print(2+2 != 10-6)
+
+result1= (2*3)*4
+print("Result 1: ", result1)
+
+
+result2 = 2 **3*4
+print("Result2 2: ", result2)
+
+result3 = 20/5*2
+print("Result 3: ", result3)
+
+result4=10-2+3
+print("Result 4: ",result4)
+
+result5=5+2**3*(4-1)
+print("Result 5:", result5)
+
