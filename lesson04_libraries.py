a2008197d18e565ac48eb02b4ce2b31ae4efae81
Julianf-30 <julian.l.fechner@gmail.com>
2025-09-30 16:27:27 +0000

    did randomness

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -1,5 +1,7 @@
 #Python libraries are ike repositories to add to your code
 
+#MATH LIBRARY
+
 import math
 print("\n--- Math Library ---\n")
 
@@ -9,4 +11,24 @@ print("Round Down:", math.floor(5.999))
 print("Power: ", math.pow(2, 5))
 print("Pi: ", math.pi)
 
-import
+# RANDOM LIBRARY
+
+import random
+seed= 86734
+
+part1=seed/150
+part2=part1*seed-450
+part3=part2/part1*.1
+part4=math.floor(part3)
+part5=part4+0.2
+print(part5)
+
+seed=54
+step1=seed/2
+step2=step1+5
+step3=step2 -20
+step4=step3-5
+print(step4)
+
+
+
