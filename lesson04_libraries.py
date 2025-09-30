81971d20db172abd73aa353cb9ea09c27bccdc97
Julianf-30 <julian.l.fechner@gmail.com>
2025-09-30 15:53:53 +0000

    added math library examples

---
+++ lesson04_libraries.py
@@ -0,0 +1,12 @@
+#Python libraries are ike repositories to add to your code
+
+import math
+print("\n--- Math Library ---\n")
+
+print("square root:", math.sqrt(49))
+print("Round Up:", math.ceil(4.001))
+print("Round Down:", math.floor(5.999))
+print("Power: ", math.pow(2, 5))
+print("Pi: ", math.pi)
+
+import
