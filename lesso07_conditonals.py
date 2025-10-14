a72570304f857c4be6ae7b84da3f59ea14619d54
Julianf-30 <julian.l.fechner@gmail.com>
2025-10-14 16:27:16 +0000

    did conditionals

---
+++ lesso07_conditonals.py
@@ -0,0 +1,65 @@
+# Conditionals in Python 
+#comparison operators: ==, !=, <, >, <=, >=
+#logical operators: and, or, not
+#control flow: if, elif, else
+
+print(3 == 2)
+print (3 != 2)
+print( "Hello" == "Hi")
+
+#if 
+num = 12 
+if num == 10:
+    print("Your number is equal to 10")
+
+#If else
+num2 = 13 
+if num2 <= 12:
+    print("Your number is less than 12")
+else:
+    print("Your number is greater than 12")
+
+#if elif else 
+temperature = 93
+if temperature > 80:
+    print("It's hot!")
+elif temperature >= 60:
+    print("It's nice out")
+else:
+    print("It's cold")
+
+x= 2 
+y=20 
+
+if x ==y: 
+    print("X is equal to y")
+elif x > y: 
+    print("X is greater than y")
+elif x < y: 
+    print("X is less than Y")
+else: 
+    print("Error")
+
+#logical operator: and 
+# Both sides of the 'and' must be true, otherwise it's false
+
+age = 16 
+has_permission = True 
+
+if age >= 17 and has_permission:
+    print("You can drive")
+else: 
+    print("No can do pal")
+
+# Using 'or' and 'not' 
+day = "sunday"
+if day == "Saturday" or day == "Sunday":
+    print("Its the weekend")
+else: 
+    print("It's a weekday")
+
+    if day is not "Monday": 
+        print("It's not Monday")
+    else: 
+        print("It's Monday")
+    
