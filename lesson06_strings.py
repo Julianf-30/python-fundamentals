06c7fe3274f7134c685627c20ba6abe14c00e63f
Julianf-30 <julian.l.fechner@gmail.com>
2025-10-14 16:28:13 +0000

    conditionals

--- lesson06_strings.py
+++ lesson06_strings.py
@@ -44,3 +44,18 @@ print(f"Hello, my name is {name}. I am {age} years old and live in {city}.")
 #f-strings can do math and function calls inside {}
 
 print(f"next year, I'll be {age+1}. My name in uppercase is {name.upper()}")
+
+
+#Challenge 1: Favorite Quote 
+
+quote = input("What is your favorite quote?: ")
+print("Your quote is", len(quote), "characters long")
+
+#Challenge 2
+first_name =input("What is your first name?: ")
+last_name= input("What is your last name?: ")
+print("You are", last_name,",", first_name)
+
+#Challenge 3
+word=input("Word?: ")
+print("Your word is: ", word.upper(), "\n Lowercase: ", word.lower(), "\n Backwards: ", word[::-1])
