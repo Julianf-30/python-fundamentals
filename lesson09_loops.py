e821deb4046575b8c5a7c6f86885fed8d2c00c44
Julianf-30 <julian.l.fechner@gmail.com>
2025-10-27 17:02:37 +0000

    did while loops

--- lesson09_loops.py
+++ lesson09_loops.py
@@ -5,21 +5,68 @@
 
 #For loops repeat for each element in a sequence (Like a list or string) 
 
-animals = ["Lamb", "Sheep", "Cow", "Goose", "Donkey"] 
+# animals = ["Lamb", "Sheep", "Cow", "Goose", "Donkey"] 
 
-import time
-for animal in animals:
-    print("Now petting:", animal)
-    time.sleep(1.5)
+# import time
+# for animal in animals:
+#     print("Now petting:", animal)
+#     time.sleep(1.5)
 
-    if animal == "Sheep":
-         print("Hi Sheep")
-print("Now I have pet all the animals")
+#     if animal == "Sheep":
+#          print("Hi Sheep")
+# print("Now I have pet all the animals")
 
-for i in range(5):
-     print("Counting", i )
+# for i in range(5):
+#      print("Counting", i )
 
 #Use start, step, step with range()
 
-for i in range(2, 11, 2):
-     print("Even numbers:", i )
+# for i in range(2, 11, 2):
+#      print("Even numbers:", i )
+
+# fav_word = "Shenanagin"
+
+# letter_list = [] 
+
+# for letter in fav_word:
+#      print(letter, end ="")
+#      letter_list.append(letter)
+#      print(letter_list)
+
+# print()
+
+# #While loops 
+# import time 
+# count = 0 
+
+# while count < 5:
+#      print(f"looping. We are on loop #{count}.")
+#      count += 1 
+#      time.sleep(0.5)
+
+# print("We have escaped the loop fr")
+
+# user_input = ""
+
+
+
+# while user_input != "exit":
+#      user_input = input("Type exit to escape: ")
+     
+# while user_input != "exit":
+#      if user_input =="exit":
+#           print("Exited")
+#           break
+
+count = 60 
+increment = 1 
+
+while count > 0:
+     count -= increment 
+     increment += 1
+     if count < 0:
+          break 
+     
+     print(count)
+
+
